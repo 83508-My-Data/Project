b4702c4c13c01be565484b6640e9ff008dba9a0e
Kd3-anikesh-84106 <anikeshghosh94@gmail.com>
2024-08-02 19:45:17 +0530

    taskcontroller added

---
+++ TaskBuddy/TaskManager/Controllers/TaskCategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskCategoryController : ControllerBase
+    {
+        private TaskBuddyContext _Context = null;
+
+        public TaskCategoryController(TaskBuddyContext context)
+        {
+                _Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<TaskCategory> Get()
+        {
+            return _Context.TaskCategories.ToList();
+
+        }
+
+        [HttpGet("{id}")]
+        public TaskCategory Get(int id)
+        {
+            return  _Context.TaskCategories.Find(id);
+        }
+
+        
+        [HttpPost]
+
+        public string Post([FromBody] TaskCategory taskCategory) {
+
+            _Context.TaskCategories.Add(taskCategory);
+            _Context.SaveChanges();
+            return "Added Successfully";
+        
+        }
+    }
+}
