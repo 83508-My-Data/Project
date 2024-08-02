d1e4a3b9adc3c3c2a653a0fe9d7be04364929ad2
kd3_83618_Akshay <akshaypatil6325@gmail.com>
2024-08-02 15:57:35 +0530

    Register UI

---
+++ Register UI/WebApplication1/Logger/Logger.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System.Text.Json;
+
+namespace WebApplication1.Logger
+{
+    public class Logger
+    {
+        private static Logger Instance = new Logger();
+        private readonly string Path = "d:\\Log\\Log.json";
+        private Logger()
+        {
+        }
+
+        public static Logger CurrentLogger { get { return Instance; } }
+
+        public void Log(string message)
+        {
+            
+            try
+            {
+                var logEntry = new LogEntry
+                {
+                    Timestamp = DateTime.Now,
+                    Message = message
+                };
+
+                
+                lock (Instance)
+                {
+                    
+                    using (var fs = new FileStream(Path, File.Exists(Path) ? FileMode.Append : FileMode.Create, FileAccess.Write))
+                    using (var writer = new StreamWriter(fs))
+                    {
+                        var json = JsonSerializer.Serialize(logEntry);
+                        writer.WriteLine(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("An error occurred while logging: " + ex.Message);
+            }
+        }
+    }
+    public class LogEntry
+    {
+        public DateTime Timestamp { get; set; }
+        public string Message { get; set; }
+    }
+}
+
