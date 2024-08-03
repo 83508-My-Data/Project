d54c976ad3f082b12972cb5b658f340ba61e6a36
KD3_Subham_83508 <subhamb41@gmail.com>
2024-08-03 17:46:11 +0530

    Registration and Update User

--- TaskBuddy/TaskManager/DTO/RegistrationDto.cs
+++ TaskBuddy/TaskManager/DTO/RegistrationDto.cs
@@ -1,28 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using TaskManager.Models;
 
-namespace TaskManager.Models
+namespace TaskManager.DTO
 {
-    public class UserDto
+    public class RegistrationDto
     {
-        public int UserId { get; set; }
-
-        
         public string FirstName { get; set; }
-
-      
         public string LastName { get; set; }
-
-        
         public string Email { get; set; }
-
-        
-
-        
+        public string Password { get; set; }
         public string MobileNo { get; set; }
-
-        
-       
         public string Address { get; set; }
+        public int RoleId { get; set; }
+        public int DeptId { get; set; }
+        public DateOnly DOB { get; set; }
     }
 }
---
+++ TaskBuddy/TaskManager/DTO/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.DTO
+{
+    public class UpdateUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public DateOnly DOB { get; set; }
+    }
+}
